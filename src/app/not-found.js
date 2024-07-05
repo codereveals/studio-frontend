89dfed849773b50991f830968c322ea31ee42919
Code Reveals <codereveals01@gmail.com>
2024-07-05 22:05:14 +0530

    404 erroe page

---
+++ src/app/not-found.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const GlobalNotFound = () => {
+    return (
+        <div>
+            <section className="bg-white dark:bg-gray-900">
+                <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                    <div className="mx-auto max-w-screen-sm text-center">
+                        <h1 className="mb-4 text-xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+                        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">Something`s missing.</p>
+                        <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">Sorry, we can not find that page. You will find lots to explore on the home page. </p>
+                        <a href="/" className=" bg-gradient-to-r from-purple-900 to-pink-800   relative h-auto inline-flex items-center justify-center rounded-full transition-colors text-sm sm:text-base font-medium py-3 px-4 sm:py-3.5 sm:px-6  ttnc-ButtonPrimary disabled:bg-opacity-90 bg-slate-900 dark:bg-slate-100 hover:bg-slate-800 text-slate-50 dark:text-slate-800 shadow-xl  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-6000 dark:focus:ring-offset-0">Back to Homepage</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default GlobalNotFound
